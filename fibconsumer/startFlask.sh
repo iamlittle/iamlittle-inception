c2a87c1a5fee7d19ff9560504d0aaa9d6c5f4fc6
iamlittle <alittle@opalytics.com>
2014-04-18 22:30:29 -0700

    renamed service and consumer

--- fibconsumer/startFlask.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-serf join $FIBAMBASSADOR_1_PORT_7946_TCP_ADDR:$FIBAMBASSADOR_1_PORT_7946_TCP_PORT
-
-python /usr/local/src/fibconsumer/fibconsumer.py
--- webapp/webapp.py
+++ webapp/webapp.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['GET'])
 def index():
-    fibip = os.popen("/usr/bin/serf members -tag role=fibservice | awk {'print $2'} | cut -d':' -f1")
+    fibip = os.popen("/usr/bin/serf members -tag role=webservice | awk {'print $2'} | cut -d':' -f1")
     fibipstr = fibip.read()
-    url = 'http://%s:5001/fib' % fibipstr
+    url = 'http://%s:5001/fib' % fibipstr.rstrip().strip()
+    sys.stderr.write('spam\n')
     response = urllib2.urlopen(url)
     html = response.read()
-    return "Hello from fibConsumer! fibService says %s" % html
+    return "Hello from webapp! fibService says %s" % html
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
